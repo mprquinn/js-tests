e4c9d13aa3cb0e1c71f4753f9a9b543fda26b054
Mike Tomothy Q <mike@simplesimple.ca>
2017-06-15 10:33:56 -0600

    second test

--- scripts/flickr-fetcher-spec.js
+++ scripts/flickr-fetcher-spec.js
@@ -1,10 +1,44 @@
 'use strict';
 
-var expect = require('chai').expect;
+const expect = require('chai').expect;
+const FlickrFetcher = require('./flickr-fetcher.js');
 
 describe('FlickrFetcher', function () {
   it ('should exist', function () {
-    var FlickrFetcher = require('./flickr-fetcher.js');
     expect(FlickrFetcher).to.not.be.undefined;
+  });
+});
+
+describe('#photoObjToURL()', function () {
+  it('should take a photo object from Flickr and return a string', function () {
+    let input = {
+      id:       '24770505034',
+      owner:    '97248275@N03',
+      secret:   '31a9986429',
+      server:   '1577',
+      farm:     2,
+      title:    '20160229090898',
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0
+    };
+    let expected = 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    let actual = FlickrFetcher.photoObjToURL(input);
+    expect(actual).to.eql(expected);
+
+    input = {
+      id:       '24770504484',
+      owner:    '97248275@N03',
+      secret:   '69dd90d5dd',
+      server:   '1451',
+      farm:     2,
+      title:    '20160229090903',
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0
+    };
+    expected = 'https://farm2.staticflickr.com/1451/24770504484_69dd90d5dd_b.jpg';
+    actual = FlickrFetcher.photoObjToURL(input);
+    expect(actual).to.eql(expected);
   });
 });
---
+++ scripts/flickr-fetcher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const FlickrFetcher = {
+    photoObjToURL: function(photoObj) {
+      return `https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${photoObj.secret}_b.jpg`;
+    }
+};
+
+module.exports = FlickrFetcher;
