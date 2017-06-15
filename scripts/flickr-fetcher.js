39f154057ebec2ee295498237c84964e57a71de2
Mike Tomothy Q <mike@simplesimple.ca>
2017-06-15 10:49:06 -0600

    transformphoto test

--- scripts/flickr-fetcher.js
+++ scripts/flickr-fetcher.js
@@ -3,7 +3,14 @@
 const FlickrFetcher = {
     photoObjToURL: function(photoObj) {
       return `https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${photoObj.secret}_b.jpg`;
-    }
+    },
+
+    transformPhotoObj: function (obj) {
+      return {
+        title: obj.title,
+        url:   `https://farm${obj.farm}.staticflickr.com/${obj.server}/${obj.id}_${obj.secret}_b.jpg`
+      };
+    },
 };
 
 module.exports = FlickrFetcher;
